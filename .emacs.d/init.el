4ab20ad4f09e5eb2e61a3867e89cfd274c73d910
Attila Sedon <attilasedon1@gmail.com>
2022-07-03 10:20:51 +0200

    .emacs.d added

---
+++ .emacs.d/init.el
@@ -0,0 +1,113 @@
+;; Useful stuff
+; :diminish -> do not appear on modeline
+; :ensure -> ensure package is available always (download if not available)
+; :custom or :config -> add customization
+; :bind -> bind key commands
+; remap -> remap keybinding to a new function
+; # -> use a function instead of the defined (default) function
+
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1) ;; Disable visible scrollbar
+(tool-bar-mode -1) ;; Disable tollbar
+(tooltip-mode -1) ; Disable tooltips
+(set-fringe-mode 10) ; Give some breathing room
+(menu-bar-mode -1) ; Disable the menubar
+
+
+(setq visible-bell t)
+(set-face-attribute 'default nil :font "Fira Code" :height 120)
+(load-theme 'wombat)
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Initialize package sources
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents (package-refresh-contents))
+
+;; initilize use-package on non linux platforms
+(unless (package-installed-p 'use-package) (package-install 'use-package))
+
+;; Always ensure that the package will be available (download it if it is not available)
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Show line numebrs
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; show command log 
+(use-package command-log-mode)
+
+;; Fuzzy search
+
+(use-package swiper)
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+;; Nicer mode bar
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom (doom-modeline-height 15))
+
+(use-package doom-themes)
+
+;; Delimiters for parenthesis
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Shows what the consecutive button combination will do
+(use-package which-key
+  :init (which-key-mode)
+  :diminish
+  :config (setq which-key-idle-delay 1))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
+  :config (setq ivy-initial-inputs-alist nil))
+
+(use-package helpful
+  :ensure t
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describle-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
