763330a37c90ce0e0a95ffaf22e627b6e10e3798
Attila Sedon <attilasedon1@gmail.com>
2022-07-05 17:13:15 +0200

    small changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -146,32 +146,32 @@
    "t" '(:ignore t :which-key "toggles")
    "tt" '(counsel-load-theme :which-key "choose-theme")))
 
-;(defun sedon/evil-hook ()
-;  (dolist (mode '(Buffer-menu-mode))
-;    (add-to-list 'evil-emacs-state-modes mode)))
-;
-;(use-package evil
-;  :init
-;  (setq evil-want-integration t)
-;  (setq evil-want-keybinding nil)
-;  (setq evil-want-C-u-scroll t)
-;  (setq evil-want-C-i-jump nil)
-;  :config
-;  (evil-mode 1)
-;  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-;  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-;
-;  ;; Use visual line motions even outside of visual-line-mode buffers
-;  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-;  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-;
-;  (evil-set-initial-state 'messages-buffer-mode 'normal)
-;  (evil-set-initial-state 'dashboard-mode 'normal))
+(defun sedon/evil-hook ()
+   (dolist (mode '(Buffer-menu-mode))
+     (add-to-list 'evil-emacs-state-modes mode)))
 
-;(use-package evil-collection
-;  :after evil
-;  :config
-;  (evil-collection-init))
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+;  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
 
 (use-package hydra)
 (defhydra hydra-buffer-menu (:color pink
@@ -204,7 +204,9 @@ _~_: modified
   ("o" Buffer-menu-other-window "other-window" :color blue)
   ("q" quit-window "quit" :color blue))
 
-(define-key Buffer-menu-mode-map "." 'hydra-buffer-menu/body)
+; (define-key Buffer-menu-mode-map "." 'hydra-buffer-menu/body))
+(rune/leader-keys
+  "b" '(hydra-buffer-menu/body :which-key "buffer menu"))
 
 (defhydra hydra-text-scale (:timeout 4)
   "scale text"
@@ -214,7 +216,7 @@ _~_: modified
 
 
 (rune/leader-keys
-  "ts" '(hydra-text-scale/body :which-key "scale text")
+  "ts" '(hydra-text-scale/body :which-key "scale text"))
 
 (use-package markdown-mode)
 (use-package flymd)
