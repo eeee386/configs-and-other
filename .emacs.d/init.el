0a5f5db408f4c48c3c8fa402eba80f6cbe2b1aec
Attila Sedon <attilasedon1@gmail.com>
2022-07-08 16:34:03 +0200

    changes added

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -40,7 +40,7 @@
 ;; set new keyboard shortcuts to call emacs functions
 ;; Make <esc> quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
+(global-set-key (kbd "M-p") 'eshell)
 
 ;; Initialize package sources
 (require 'package)
@@ -62,7 +62,7 @@
 (global-display-line-numbers-mode t)
 
 (dolist (mode '(org-mode-hook
-		term-mode-hook
+			term-mode-hook
 		eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
@@ -160,7 +160,7 @@
   (setq evil-want-C-u-scroll t)
   (setq evil-want-C-i-jump nil)
   :hook
-  (sedon/evil-hook)
+  (sedon/evil-hook t)
   :config
   (evil-mode 1)
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
@@ -171,6 +171,7 @@
   (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
   (evil-set-initial-state 'messages-buffer-mode 'normal)
+  
   (evil-set-initial-state 'dashboard-mode 'normal))
 
 (use-package evil-collection
@@ -178,7 +179,8 @@
   :config
   (evil-collection-init))
 
-(use-package hydra)
+(use-package hydra
+  :bind-keymap ())
 (defhydra hydra-buffer-menu (:color pink
                              :hint nil)
   "
@@ -209,7 +211,7 @@ _~_: modified
   ("o" Buffer-menu-other-window "other-window" :color blue)
   ("q" quit-window "quit" :color blue))
 
-; (define-key Buffer-menu-mode-map "." 'hydra-buffer-menu/body))
+(define-key Buffer-menu-mode-map "." 'hydra-buffer-menu/body)
 (rune/leader-keys
   "b" '(hydra-buffer-menu/body :which-key "buffer menu"))
 
@@ -248,3 +250,60 @@ _~_: modified
 (use-package magit
   ;; put diff buffer in the same buffer as the code
   :custom (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+;; Git helper for handling pull requests, issues, notifications
+(use-package forge)
+
+
+(defun efs/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+    (visual-line-mode 1))
+
+
+(defun efs/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Cantarell" :weight 'bold :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . efs/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾"
+	org-hide-emphasis-markers t)
+  (efs/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun efs/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . efs/org-mode-visual-fill))
